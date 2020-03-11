9f3ea1c2f02cf7d434dbce9a2e20eaf5b4a21ad0
kmarsala <kmarsala5@hotmail.com>
2020-03-11 10:44:12 -0400

    adding ant custom theme setup

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,97 +1,100 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Breakpoint } from 'react-socks';
+import { Button } from 'antd';
+
 import './Dashboard.scss';
 
 const Dashboard = () => {
-    return (
-      <>
-        <Breakpoint large up>
-          <div className="dashboard" data-testid="main__dashboard">
-            <div className="dashboard-card-wrapper">
-              <NavLink to="/nowplaying">
-                <div>
-                  <i className="fa fa-ticket" aria-hidden="true" />
-                  <h1>Now Playing</h1>
-                </div>
-              </NavLink>
-            </div>
-            <div className="dashboard-card-wrapper">
-              <NavLink to="/upcoming">
-                <div>
-                  <i className="fa fa-video-camera" aria-hidden="true" />
-                  <h1>Upcoming Releases</h1>
-                </div>
-              </NavLink>
-            </div>
-            <div className="dashboard-card-wrapper">
-              <NavLink to="/genres">
-                <div>
-                  <i className="fa fa-film" aria-hidden="true" />
-                  <h1>Genres</h1>
-                </div>
-              </NavLink>
-            </div>
+  return (
+    <>
+      <Button type="primary">Button</Button>
+      <Breakpoint large up>
+        <div className="dashboard" data-testid="main__dashboard">
+          <div className="dashboard-card-wrapper">
+            <NavLink to="/nowplaying">
+              <div>
+                <i className="fa fa-ticket" aria-hidden="true" />
+                <h1>Now Playing</h1>
+              </div>
+            </NavLink>
           </div>
-        </Breakpoint>
-        <Breakpoint medium only>
-          <div className="dashboard-tablet" data-testid="main__dashboard">
-            <div className="dashboard-card-wrapper">
-              <NavLink to="/nowplaying">
-                <div>
-                  <i className="fa fa-ticket" aria-hidden="true" />
-                  <h1>Now Playing</h1>
-                </div>
-              </NavLink>
-            </div>
-            <div className="dashboard-card-wrapper">
-              <NavLink to="/upcoming">
-                <div>
-                  <i className="fa fa-video-camera" aria-hidden="true" />
-                  <h1>Upcoming Releases</h1>
-                </div>
-              </NavLink>
-            </div>
-            <div className="dashboard-card-wrapper">
-              <NavLink to="/genres">
-                <div>
-                  <i className="fa fa-film" aria-hidden="true" />
-                  <h1>Genres</h1>
-                </div>
-              </NavLink>
-            </div>
+          <div className="dashboard-card-wrapper">
+            <NavLink to="/upcoming">
+              <div>
+                <i className="fa fa-video-camera" aria-hidden="true" />
+                <h1>Upcoming Releases</h1>
+              </div>
+            </NavLink>
           </div>
-        </Breakpoint>
-        <Breakpoint small down>
-          <div className="dashboard-mobile" data-testid="main__dashboard">
-            <div className="dashboard-card-wrapper">
-              <NavLink to="/nowplaying">
-                <div>
-                  <i className="fa fa-ticket" aria-hidden="true" />
-                  <h1>Now Playing</h1>
-                </div>
-              </NavLink>
-            </div>
-            <div className="dashboard-card-wrapper">
-              <NavLink to="/upcoming">
-                <div>
-                  <i className="fa fa-video-camera" aria-hidden="true" />
-                  <h1>Upcoming Releases</h1>
-                </div>
-              </NavLink>
-            </div>
-            <div className="dashboard-card-wrapper">
-              <NavLink to="/genres">
-                <div>
-                  <i className="fa fa-film" aria-hidden="true" />
-                  <h1>Genres</h1>
-                </div>
-              </NavLink>
-            </div>
+          <div className="dashboard-card-wrapper">
+            <NavLink to="/genres">
+              <div>
+                <i className="fa fa-film" aria-hidden="true" />
+                <h1>Genres</h1>
+              </div>
+            </NavLink>
           </div>
-        </Breakpoint>
-      </>
-    );
-  }
+        </div>
+      </Breakpoint>
+      <Breakpoint medium only>
+        <div className="dashboard-tablet" data-testid="main__dashboard">
+          <div className="dashboard-card-wrapper">
+            <NavLink to="/nowplaying">
+              <div>
+                <i className="fa fa-ticket" aria-hidden="true" />
+                <h1>Now Playing</h1>
+              </div>
+            </NavLink>
+          </div>
+          <div className="dashboard-card-wrapper">
+            <NavLink to="/upcoming">
+              <div>
+                <i className="fa fa-video-camera" aria-hidden="true" />
+                <h1>Upcoming Releases</h1>
+              </div>
+            </NavLink>
+          </div>
+          <div className="dashboard-card-wrapper">
+            <NavLink to="/genres">
+              <div>
+                <i className="fa fa-film" aria-hidden="true" />
+                <h1>Genres</h1>
+              </div>
+            </NavLink>
+          </div>
+        </div>
+      </Breakpoint>
+      <Breakpoint small down>
+        <div className="dashboard-mobile" data-testid="main__dashboard">
+          <div className="dashboard-card-wrapper">
+            <NavLink to="/nowplaying">
+              <div>
+                <i className="fa fa-ticket" aria-hidden="true" />
+                <h1>Now Playing</h1>
+              </div>
+            </NavLink>
+          </div>
+          <div className="dashboard-card-wrapper">
+            <NavLink to="/upcoming">
+              <div>
+                <i className="fa fa-video-camera" aria-hidden="true" />
+                <h1>Upcoming Releases</h1>
+              </div>
+            </NavLink>
+          </div>
+          <div className="dashboard-card-wrapper">
+            <NavLink to="/genres">
+              <div>
+                <i className="fa fa-film" aria-hidden="true" />
+                <h1>Genres</h1>
+              </div>
+            </NavLink>
+          </div>
+        </div>
+      </Breakpoint>
+    </>
+  );
+}
 
 export default Dashboard;
